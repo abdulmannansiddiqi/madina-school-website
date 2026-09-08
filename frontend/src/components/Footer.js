a031ffe956d6f4ccfb27c39460d3a9e0fa65b626
abdulmannansiddiqi <abdulmannansiddiqi7@gmail.com>
2026-09-08 12:30:53 +0500

    Add Navbar, Footer components and basic Home page

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer>
+      <p>&copy; 2026 Madina Model Higher Secondary School, Faqirwali</p>
+      <p>PEF Registered | Free Education for All</p>
+      <p>Contact: 03462569709 | themmps786@gmail.com</p>
+    </footer>
+  );
+}
---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,18 @@
+export default function Navbar() {
+  return (
+    <nav>
+      <div>
+        <strong>Madina Model Higher Secondary School</strong>
+      </div>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Academics</li>
+        <li>Admissions</li>
+        <li>Gallery</li>
+        <li>News</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
+}
